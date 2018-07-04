ca279e1158ee991bac58f3b906e36e032cddd462
Martin Papy <martin.papy@gmail.com>
2018-07-04 15:08:21 +0700

    Fixed on MacOS

--- src/clone-jira.sh
+++ src/clone-jira.sh
@@ -3,14 +3,14 @@
 # loading external configuration
 source ./lib/config.shlib;
 
-RSYNC_OPTIONS=$(config_get RSYNC_OPTIONS)
-RSYNC_SSH_OPTIONS=$(config_get RSYNC_SSH_OPTIONS)
+RSYNC_OPTIONS="$(config_get RSYNC_OPTIONS)"
+RSYNC_SSH_OPTIONS="$(config_get RSYNC_SSH_OPTIONS)"
 
-RSYNC_SRC_JIRA_FOLDER=$(config_get RSYNC_SRC_JIRA_FOLDER)
-RSYNC_DST_JIRA_FOLDER=$(config_get RSYNC_DST_JIRA_FOLDER)
+RSYNC_SRC_JIRA_FOLDER="$(config_get RSYNC_SRC_JIRA_FOLDER)"
+RSYNC_DST_JIRA_FOLDER="$(config_get RSYNC_DST_JIRA_FOLDER)"
 
-RSYNC_SRC_JIRA_DATA_FOLDER=$(config_get RSYNC_SRC_JIRA_DATA_FOLDER)
-RSYNC_DST_JIRA_DATA_FOLDER=$(config_get RSYNC_DST_JIRA_DATA_FOLDER)
+RSYNC_SRC_JIRA_DATA_FOLDER="$(config_get RSYNC_SRC_JIRA_DATA_FOLDER)"
+RSYNC_DST_JIRA_DATA_FOLDER="$(config_get RSYNC_DST_JIRA_DATA_FOLDER)"
 
 # First we need to stop local services
 /etc/init.d/jira stop
@@ -24,16 +24,16 @@ fi
 # Now we sync all data from production to local folders
 echo "Synching JIRA Program files from Production to Local Folder"
 if [ ${#RSYNC_SSH_OPTIONS} -eq 0 ] ; then
-    rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
+   rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
 else
-    rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
+   rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
 fi
 
 echo "Synching JIRA Data files from Production to Local Folder"
 if [ ${#RSYNC_SSH_OPTIONS} -eq 0 ] ; then
-    rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_DATA_FOLDER} ${RSYNC_DST_JIRA_DATA_FOLDER}
+   rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_DATA_FOLDER} ${RSYNC_DST_JIRA_DATA_FOLDER}
 else
-    rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_DATA_FOLDER} ${RSYNC_DST_JIRA_DATA_FOLDER}
+   rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_DATA_FOLDER} ${RSYNC_DST_JIRA_DATA_FOLDER}
 fi
 
 # Now we need to cleanup the Configuration files
