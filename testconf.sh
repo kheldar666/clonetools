0446139bcce08991075b39e3a9d0b11b74d12a2b
Martin Papy <martin.papy@gmail.com>
2018-07-04 10:47:35 +0700

    refactoring

--- testconf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source ./config.shlib; # load the config library functions
-echo "$(config_get RSYNC_OPTIONS)"; # will be found in user-cfg
-printf -- "%s\n" "$(config_get RSYNC_OPTIONS)"; # safer way of echoing!
-echo "$(config_get RSYNC_DST_JIRA_FOLDER)"; # will fall back to defaults
-echo "$(config_get bleh)"; # "__UNDEFINED__" since it isn't set anywhere
