f808740e90c03e5809214ffa2d99201ac4e07660
Martin Papy <martin.papy@gmail.com>
2018-07-04 10:10:06 +0700

    Testing another value

--- testconf.sh
+++ testconf.sh
@@ -2,5 +2,5 @@
 source ./config.shlib; # load the config library functions
 echo "$(config_get RSYNC_OPTIONS)"; # will be found in user-cfg
 printf -- "%s\n" "$(config_get RSYNC_OPTIONS)"; # safer way of echoing!
-echo "$(config_get RSYNC_SSH_OPTIONS)"; # will fall back to defaults
+echo "$(config_get RSYNC_DST_JIRA_FOLDER)"; # will fall back to defaults
 echo "$(config_get bleh)"; # "__UNDEFINED__" since it isn't set anywhere
