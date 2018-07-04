0e9d4c7123e77866442979862ef0ff7bb0818fe9
Martin Papy <martin.papy@gmail.com>
2018-07-04 14:34:01 +0700

    Adjust test

--- test/test-rsync.sh
+++ test/test-rsync.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-RSYNC_OPTIONS="-rav --delete"
+RSYNC_OPTIONS=-rav --delete
 RSYNC_SSH_OPTIONS=""
 
 RSYNC_SRC_JIRA_FOLDER=someuser@somehost:/path/to/jira
 RSYNC_DST_JIRA_FOLDER=/path/to/local/folder/jira
 
 # Tetsing RSYNC
-if [ ${RSYNC_SSH_OPTIONS} == '' ] ; then
+if [ ${#RSYNC_SSH_OPTIONS} -eq 0 ] ; then
     rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
 else
     rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
