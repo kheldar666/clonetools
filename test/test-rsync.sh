e21a24c62f670d3cd0eb34500db769eafa30a85e
Martin Papy <martin.papy@gmail.com>
2018-07-04 14:10:42 +0700

    Adjust the script to support external config

--- test/test-rsync.sh
+++ test/test-rsync.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-RSYNC_OPTIONS=-rav --delete
-RSYNC_SSH_OPTIONS=
+RSYNC_OPTIONS="-rav --delete"
+RSYNC_SSH_OPTIONS=""
 
 RSYNC_SRC_JIRA_FOLDER=someuser@somehost:/path/to/jira
 RSYNC_DST_JIRA_FOLDER=/path/to/local/folder/jira
 
-# Now we sync all data from production to local folders
-echo "Synching JIRA Program files from Production to Local Folder"
-rsync $RSYNC_OPTIONS $RSYNC_SSH_OPTIONS $RSYNC_SRC_JIRA_FOLDER $RSYNC_DST_JIRA_FOLDER
+# Tetsing RSYNC
+if [ ${RSYNC_SSH_OPTIONS} == '' ] ; then
+    rsync ${RSYNC_OPTIONS} ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
+else
+    rsync ${RSYNC_OPTIONS} -e "${RSYNC_SSH_OPTIONS}" ${RSYNC_SRC_JIRA_FOLDER} ${RSYNC_DST_JIRA_FOLDER}
+fi
